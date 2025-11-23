642b99ccfe01ec842162b94d32a19a321e20232d
kesaitou <ksaito1mini@gmail.com>
2025-11-23 18:50:25 +0900

    expand variables

--- srcs/parser/expand1_expand_args.c
+++ srcs/parser/expand1_expand_args.c
@@ -6,19 +6,58 @@
 /*   By: kesaitou <kesaitou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 15:51:16 by natakaha          #+#    #+#             */
-/*   Updated: 2025/11/23 17:01:10 by kesaitou         ###   ########.fr       */
+/*   Updated: 2025/11/23 18:50:05 by kesaitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/execution.h"
 #include "../../includes/parser.h"
 
+static int is_delimiter_variables(int c)
+{
+	return (c != '_' || !ft_isalnum(c));
+}
+
+static	int count_varibles(char *av)
+{
+	int	len;
+
+	len = 0;
+	while (*av && is_delimiter_variables(*av))
+		len++;
+	return (len);
+}
+
+int hundle_expand_var(t_token **cur_list, char **cur_argv, t_state state, char *shell_var)
+{
+	char	*var;
+	char	*tmp;
+	int		var_name_len;
+
+	(void)shell_var;
+	var_name_len = count_varibles(*cur_argv);
+	tmp = ft_strndup(*cur_argv, var_name_len);
+	if (!tmp)
+		return (FAILUER);
+	var = getenv(tmp);
+	if (!var)
+		return (free(tmp), SUCCESS);
+	free((*cur_list) ->token); 
+	free(tmp);
+	tmp = ft_strdup(var);
+	if (!tmp)
+		return (FAILUER);
+	(*cur_list) ->token = tmp;
+	(*cur_argv) += var_name_len;
+	return (free(tmp), SUCCESS);
+}
+
 static void	manage_quote_expander(char **av, t_state *state)
 {
 	if (**av == '\'')
 	{
 		*state = STATE_SQUOTE;
-		
+		(*av)++;
 	}
 	else if (**av == '"')
 	{
@@ -27,40 +66,59 @@ static void	manage_quote_expander(char **av, t_state *state)
 	}
 }
 
-int manage_state_general_expander(char **av, t_state *state)
+int manage_state_dquote(t_token **cur_list, char **cur_argv, t_state *state)
 {
-	manage_quote_expander(av, state);
-	if (state != STATE_GENERAL)
+	if (**cur_argv == '$')
 	{
-		/* code */
+		(*cur_argv)++;
+		if (hundle_expand_var(cur_list, cur_argv, *state, NULL) == FAILUER)
+			return (FAILUER);
 	}
-	
-
-
+	else
+		(*cur_argv)++;
+	return (SUCCESS);
 }
 
-t_token *state_transition(char *av)
+int manage_state_general_expander(t_token **cur_list, char **cur_argv, t_state *state)
 {
+	manage_quote_expander(cur_argv, state);
+	if (state != STATE_GENERAL)
+		return (SUCCESS);
+	if (**cur_argv == '$')
+	{
+		(*cur_argv)++;
+		if (hundle_expand_var(cur_list, cur_argv, *state, NULL) == FAILUER)
+			return (FAILUER);
+	}
+	else
+		(*cur_argv)++;
+	return (SUCCESS);
+}
+
+int  manage_state_transition_expander(t_token **cur_list)
+{
+	char	*cur_argv;
 	t_state state;
 
+	if (!ft_strchr((*cur_list) ->token, '$'))
+			return (SUCCESS);
 	state = STATE_GENERAL;
-	while (*av)
+	cur_argv = (*cur_list) ->token;
+	while (*cur_argv)
 	{
 		if (state == STATE_GENERAL)
 		{
-			if (manage_state_general_expander(&av, state) == FAILUER)
-				return (FAILUER);
-		}
-		else if (state == STATE_SQUOTE)
-		{
-			if (manage_state_squote(input, state, c_list) == FAILUER)
+			if (manage_state_general_expander(cur_list, &cur_argv, state) == FAILUER)
 				return (FAILUER);
 		}
 		else if (state == STATE_DQUOTE)
 		{
-			if (manage_state_dquote(input, state, c_list) == FAILUER)
-				return (FAILUER);
+
+			
 		}
+		else if (state == STATE_SQUOTE)
+			(*cur_argv)++;		
+		
 	}
 	return (SUCCESS);
 	
@@ -69,24 +127,20 @@ t_token *state_transition(char *av)
 	
 }
 
-t_token	*expand_args(t_token *token_list)
+int	*expand_variables(t_token **token_list)
 {
-	t_token *new_token_list;
+	t_token *cur_list;
 
-	new_token_list = NULL;
-	while (token_list)
+	if (!token_list)
+		return (FAILUER);
+	cur_list = *token_list;
+	while (cur_list)
 	{
-		new_token_list = state_transition(token_list ->token);
-		
-		
-		
-		
+		if (manage_state_transition_expander(&cur_list) == FAILUER)
+			return (FAILUER);
+		cur_list = cur_list ->next;
 	}
-	
-	
-	
-	
-	
+	return (SUCCESS);
 	
 }
 
