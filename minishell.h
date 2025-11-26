58f708d8a5918f8d9b029fa97777895429ed65b0
Keisuke Saitou <kesaitou@c5r1s2.42tokyo.jp>
2025-11-26 19:43:01 +0900

    minishell test-tree

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kesaitou <kesaitou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 12:40:22 by kesaitou          #+#    #+#             */
-/*   Updated: 2025/11/26 13:41:29 by kesaitou         ###   ########.fr       */
+/*   Updated: 2025/11/26 18:40:27 by kesaitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@
 typedef struct s_tree
 {
 	char			*op;
-	struct s_node	*left;
-	struct s_node	*right;
+	struct s_tree	*left;
+	struct s_tree	*right;
 
 }					t_tree;
-
 
 
 #endif
--- test_tree.c
+++ test_tree.c
@@ -1,32 +1,124 @@
 #include "minishell.h"
 #include <stdio.h>
+/*
 
-t_tree	*create_tree(void)
+	typedef struct s_tree
+	{
+		char			*op;
+		struct s_tree	*left;
+		struct s_tree	*right;
+
+	}					t_tree;
+
+	オペレーターが演算子
+	文字列の中から演算子（キーワード）を探索し、オペレーターを親にしてオペランドを子にとる
+
+	オペレーターが来たら左右の子供を探索する。左の子も存在するとは限らない
+	右の子は次のオペレーターが来るまで探索する。
+	
+	-エラー条件
+	オペレーターに対して子供が２つ存在しない
+
+	オペレータが存在しないケース
+
+
+	その時点の親の
+
+
+*/
+
+
+
+
+t_tree	*create_node(void)
 {
-	t_tree	*tree;
+	t_tree	*node;
 
-	tree = malloc(sizeof(t_tree));
-	if (!tree)
+	node = malloc(sizeof(t_tree));
+	if (!node)
 		return (NULL);
-		
-		
-	tree->right = NULL;
-	tree->op = "aaabbbbba";
+	node ->left = NULL;
+	node->right = NULL;
+	node->op = NULL;
+	return (node);
 }
 
-
-
-void	create_left_child(t_tree *tree)
+t_tree	*create_tree(t_tree *tree, int len)
 {
-	tree ->left = create_tree();
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		tree 
+	}
+	
+	
+	
+	
+	
+	
+}
+
+void	store_child(t_tree *tree)
+{
+	
+	
+	
+}
+
+int	count_op(char *input)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (*input)
+	{
+		if (*input == '|' || *input == '>')
+		{
+			cnt++;
+		}
+		input++;
+	}
+	return (cnt);
+}
+
+int	serach_op(char *input, t_tree *tree)
+{
+	while (*input)
+	{
+		if (*input == '|')
+		{
+			store_child()
+		}
+		else if (*input == '>')
+		{
+
+		}
+	
+
+		
+	}
+	
+	
+	
+	
+	
 }
 
 int	main(void)
 {
 	t_tree *tree;
+	char	*test1 = "grep -v aaaa | cat -e | cat > outfile";
 
+
+	
+	
+	
+	
 	tree = create_tree();
-	create_left_child(&tree);
-	tree ->left.
+	create_left_child(tree);
+	create_left_child(tree ->left);
+	printf("%s",tree ->left ->left ->op);
 	
 }
