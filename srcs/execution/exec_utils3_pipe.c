d0f42a77d290a3787a514c281580edbafbbcfa6b
naoki <ai.business.naokix@gmail.com>
2025-12-14 23:25:03 +0900

    latest

--- srcs/execution/exec_utils3_pipe.c
+++ srcs/execution/exec_utils3_pipe.c
@@ -12,60 +12,6 @@
 
 #include "../../includes/execution.h"
 
-/*initial num of fd == -1*/
-
-//int	pipe_update(int	fd_in[2], int fd_out[2])
-//{
-//	if (fd_in[0] >= 0)
-//	{
-//		close(fd_in[0]);
-//		fd_in[0] = -1;
-//	}
-//	if (fd_in[1] >= 0)
-//	{
-//		close(fd_in[1]);
-//		fd_in[1] = -1;
-//	}
-//	if (fd_out[0] >= 0)
-//		fd_in[0] = fd_out[0];
-//	if (fd_out[1] >= 0)
-//		fd_in[1] = fd_out[1];
-//	if (pipe(fd_out) == FAILUER)
-//		return (FAILUER);
-//	return (SUCCESS);
-//}
-
-//int	pipe_terminate(int	fd_in[2], int fd_out[2])
-//{
-//	if (fd_in[0] >= 0)
-//	{
-//		close(fd_in[0]);
-//		fd_in[0] = -1;
-//	}
-//	if (fd_in[1] >= 0)
-//	{
-//		close(fd_in[1]);
-//		fd_in[1] = -1;
-//	}
-//	if (fd_out[0] >= 0)
-//		fd_in[0] = fd_out[0];
-//	if (fd_out[1] >= 0)
-//		fd_in[1] = fd_out[1];
-//	fd_out[0] = -1;
-//	fd_out[1] = -1;
-//	return (SUCCESS);
-//}
-
-//void	close_fd_in_out(int *fd_in, int *fd_out)
-//{
-//	if (*fd_in >= 0)
-//		close(*fd_in);
-//	*fd_in = -1;
-//	if (*fd_out >= 0)
-//		close(*fd_out);
-//	*fd_out = -1;
-//}
-
 void	close_unused_pipe(int fd_in, int fd_out, int pipes[2])
 {
 	if (pipes[0] >= 0 && pipes[0] != fd_in)
