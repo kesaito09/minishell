553f0b87441a5b1a0419c3c849881236ff90afc8
dannmitsu Official <ai.business.naokix@gmail.com>
2026-01-01 20:51:25 +0000

    atty mode

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,7 +6,7 @@
 /*   By: natakaha <natakaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 11:36:49 by natakaha          #+#    #+#             */
-/*   Updated: 2025/12/30 13:00:07 by natakaha         ###   ########.fr       */
+/*   Updated: 2026/01/01 20:50:21 by natakaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,92 @@
 #include "../../includes/execution.h"
 #include "../../includes/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
+char	*get_line(int fd)
+{
+	char	*tmp;
+	char	*trash;
+	char	*line;
+
+	tmp = "tmp";
+	line = NULL;
+	while (tmp)
+	{
+		trash = line;
+		tmp = get_next_line(fd);
+		line = ft_strjoin(line, tmp);
+		free(tmp);
+		free(trash);
+		if (!line)
+			return (NULL);
+	}
+	return (line);
+}
+
+int	minishell_atty(t_pipe *info)
 {
 	char	*line;
 	t_tree	*branch;
-	t_pipe	info;
+	int		flag;
 
-	info = collect_info(envp);
-	while (1)
+	flag = SUCCESS;
+	while (true)
 	{
 		setup_signal_prompt();
 		line = readline("minishell$ ");
 		if (!line)
-			return (rl_clear_history(), 0);
+			return (rl_clear_history(), SUCCESS);
+		if (!*line)
+			continue ;
 		add_history(line);
-		branch = parser(line, &info);
-		setup_signal_exec();
-		tree_operator(branch, &info, 0, 1);
-		free_tree_rec(branch);
-		info.ecode = waitpid_plist(&info.plist);
+		branch = parser(line, info);
 		free(line);
-		info.pipe = false;
+		if (!branch)
+			return (rl_clear_history(), FAILUER);
+		setup_signal_exec();
+		flag = tree_operator(branch, info, 0, 1);
+		free_tree_rec(branch);
+		info->ecode = waitpid_plist(&info->plist);
+		info->pipe = false;
 	}
+	return (flag);
+}
+
+int	minishell_pipe(t_pipe *info)
+{
+	char	*line;
+	t_tree	*branch;
+	int		flag;
+
+	line = get_line(STDIN_FILENO);
+	if (!line)
+		return (FAILUER);
+	branch = parser(line, info);
+	if (!branch)
+		return (free(line), FAILUER);
+	flag = tree_operator(branch, info, 0, 1);
+	free_tree_rec(branch);
+	info->ecode = waitpid_plist(&info->plist);
+	free(line);
+	return (flag);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_pipe	info;
+	int		flag;
+
+	info = collect_info(envp);
+	if (!isatty(STDIN_FILENO))
+		flag = minishell_pipe(&info);
+	else
+		flag = minishell_atty(&info);
 	free_split(info.path);
+	t_lstclear(&info.envp, free);
 	(void)argc;
 	(void)argv;
+	if (flag == FAILUER)
+		return (1);
+	return (0);
 }
 
 // /*tester*/
